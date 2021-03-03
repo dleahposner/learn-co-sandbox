ddea4611a4f40623742791d45998df7adb77aeab
Devorah Posner <dleahposner@gmail.com>
2021-03-03 23:50:48 +0000

    Automatically backed up by Learn

---
+++ birthday.rb
@@ -0,0 +1,11 @@
+num_of_attendees = 547
+puts "there are #{num_of_attendees} people coming to Beyonce's birthday party."
+
+def restaurant
+  restaurant_name = "Guy's American Kitchen & Bar"
+  cuisine = "american"
+  motto = "Welcome to Flavor Town!"
+end
+
+restaurant
+
---
+++ my_name.rb
@@ -0,0 +1,5 @@
+puppies = ["bulldog". "terrier", "poodle"]
+
+random_numbers = [2, 5, 6, 8, 30050]
+
+mixed = ["this", "array", 7 ,20, "has" , 45, "&", "strings", 309]
